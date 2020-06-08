c8cbc9eeade909e859ce54ee3cd2c8c5f598f79b
jaredcassoutt <65668106+jaredcassoutt@users.noreply.github.com>
2020-06-08 10:37:05 -0500

    Update AnagramChecker.py

--- AnagramChecker.py
+++ AnagramChecker.py
@@ -5,12 +5,12 @@
  in the same quantity. Only consider characters, not spaces
  or punctuation.  Consider capital letters to be the same as lower case
  --- Examples
-   anagrams('rail safety', 'fairy tales') --> True
-   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-   anagrams('Hi there', 'Bye there') --> False
+   checkAnagram('rail safety', 'fairy tales') --> True
+   checkAnagram('RAIL! SAFETY!', 'fairy tales') --> True
+   checkAnagram('Hi there', 'Bye there') --> False
 '''
 
-def checkAnagran(str1,str2):
+def checkAnagram(str1,str2):
     editStr1 = sorted(''.join(e for e in str1 if e.isalnum() and e.isalpha()).lower())
     editStr2 = sorted(''.join(e for e in str2 if e.isalnum() and e.isalpha()).lower())
     return editStr1 == editStr2
